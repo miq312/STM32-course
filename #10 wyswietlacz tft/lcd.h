c074a5cae9e32305265dae98e461672e7aae4e5e
Michał Łazarz <118307289+miq312@users.noreply.github.com>
2023-09-24 15:58:22 +0200

    Add files via upload

---
+++ #10 wyswietlacz tft/lcd.h
@@ -0,0 +1,27 @@
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+#pragma once
+
+#include <stdint.h>
+
+#define LCD_WIDTH	160
+#define LCD_HEIGHT	128
+
+#define BLACK			0x0000
+#define RED				0x00f8
+#define GREEN			0xe007
+#define BLUE			0x1f00
+#define YELLOW			0xe0ff
+#define MAGENTA			0x1ff8
+#define CYAN			0xff07
+#define WHITE			0xffff
+
+void lcd_fill_box(int x, int y, int width, int height, uint16_t color);
+void lcd_init(void);
+void lcd_put_pixel(int x, int y, uint16_t color);
+void lcd_draw_image(int x, int y, int width, int height, const uint8_t* data);
+void lcd_copy(void);
+
+
+#endif /* INC_LCD_H_ */
